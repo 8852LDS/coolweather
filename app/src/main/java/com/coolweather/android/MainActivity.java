a4f4e0bb55afe3f8a08dc90e1be88ba00698dc88
8852LDS <1556915347@qq.com>
2018-08-24 18:40:18 +0800

    完成遍历省市县三级列表功能。

--- app/src/main/java/com/coolweather/android/MainActivity.java
+++ app/src/main/java/com/coolweather/android/MainActivity.java
@@ -1,7 +1,11 @@
 package com.coolweather.android;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import org.litepal.LitePal;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        LitePal.getDatabase() ;
+
     }
 }
