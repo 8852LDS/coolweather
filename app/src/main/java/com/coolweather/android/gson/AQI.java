93eec6fd0fd5b16e5c8dd1fc76cee1a9476bef43
8852LDS <1556915347@qq.com>
2018-08-25 16:04:37 +0800

    加入显示天气信息的功能

---
+++ app/src/main/java/com/coolweather/android/gson/AQI.java
@@ -0,0 +1,12 @@
+package com.coolweather.android.gson;
+
+/**
+ * Created by Administrator on 2018/8/25 0025.
+ */
+public class AQI {
+    public AQICity city ;
+    public class AQICity{
+        public String api ;
+        public String pm25 ;
+    }
+}
